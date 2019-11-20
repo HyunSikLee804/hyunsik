38dce6177f36d56006a23031741229fd873909b3
Hyunsik <yoseb804@gmail.com>
2019-11-20 16:42:14 +0900

    m

---
+++ 191008_prac.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	
+	int x1, y1, x2, y2, r1, r2;
+	int x3, y3;
+	int T;
+	int i;
+
+	scanf("%d", &T);
+	
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+	}
+
+
+
+
+
+	return 0;
+}
